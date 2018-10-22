1e2d7a4b91b2769698df228599063f9e744c3b71
deepbrain <artyom@gmail.com>
2018-10-21 21:08:51 -0700

    Fixed a bug in the replay memory

--- memory.py
+++ memory.py
@@ -127,8 +127,7 @@ class ReplayMemory():
     probs, idxs, tree_idxs, states, actions, returns, next_states, nonterminals = zip(*batch)
     states, next_states, = np.stack(states), np.stack(next_states)
     actions, returns, nonterminals = torch.cat(actions), torch.cat(returns), torch.stack(nonterminals)
-    probs = np.array(probs, dtype = np.float32)/p_total #, dtype=torch.float32, device=self.device).
-    #/ torch.tensor(p_total, dtype=torch.float32, device=self.device)  # Calculate normalised probabilities
+    probs = np.array(probs, dtype = np.float32)/p_total # Calculate normalised probabilities
     capacity = self.capacity if self.transitions.full else self.transitions.index
     weights = (capacity * probs) ** -self.priority_weight  # Compute importance-sampling weights w
     weights = torch.tensor(weights / weights.max(), dtype=torch.float32, device=self.device)   # Normalise by max importance-sampling weight from batch
