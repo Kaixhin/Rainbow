357c703b2724368a0062cf359b3e9333f20d85f2
Kaixhin <design@kaixhin.com>
2018-12-24 22:13:48 +0000

    Revert numpy usage for priority storage and max

--- memory.py
+++ memory.py
@@ -15,7 +15,7 @@ class SegmentTree():
     self.full = False  # Used to track actual capacity
     self.sum_tree = np.zeros((2 * size - 1, ), dtype=np.float32)  # Initialise fixed size tree with all (priority) zeros
     self.data = np.array([None] * size)  # Wrap-around cyclic buffer
-    self.max = np.array([1], dtype=np.int32)  # Initial max value to return (1 = 1^ω)
+    self.max = 1  # Initial max value to return (1 = 1^ω)
 
   # Propagates value up tree given a tree index
   def _propagate(self, index, value):
@@ -27,16 +27,16 @@ class SegmentTree():
 
   # Updates value given a tree index
   def update(self, index, value):
-    self.sum_tree[index] = np.array(value, dtype=np.int32)  # Set new value
+    self.sum_tree[index] = value  # Set new value
     self._propagate(index, value)  # Propagate value
-    self.max = np.maximum(value, self.max)
+    self.max = max(value, self.max)
 
   def append(self, data, value):
     self.data[self.index] = data  # Store data in underlying data structure
     self.update(self.index + self.size - 1, value)  # Update tree
     self.index = (self.index + 1) % self.size  # Update index
     self.full = self.full or self.index == 0  # Save when capacity reached
-    self.max = np.maximum(value, self.max)
+    self.max = max(value, self.max)
 
   # Searches for the location of a value in sum tree
   def _retrieve(self, index, value):
