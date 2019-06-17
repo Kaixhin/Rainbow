bdf8e39d675c721c3d4f9c6aebfdd7084b15566b
Kaixhin <design@kaixhin.com>
2019-06-17 10:20:29 +0100

    Use 'VALID' padding
    Implementation detail from @mtthss

--- model.py
+++ model.py
@@ -51,13 +51,13 @@ class DQN(nn.Module):
     self.action_space = action_space
 
     if args.architecture == 'canonical':
-      self.convs = nn.Sequential(nn.Conv2d(args.history_length, 32, 8, stride=4, padding=1), nn.ReLU(),
-                                 nn.Conv2d(32, 64, 4, stride=2), nn.ReLU(),
-                                 nn.Conv2d(64, 64, 3, stride=1), nn.ReLU())
+      self.convs = nn.Sequential(nn.Conv2d(args.history_length, 32, 8, stride=4, padding=0), nn.ReLU(),
+                                 nn.Conv2d(32, 64, 4, stride=2, padding=0), nn.ReLU(),
+                                 nn.Conv2d(64, 64, 3, stride=1, padding=0), nn.ReLU())
       self.conv_output_size = 3136
     elif args.architecture == 'data-efficient':
-      self.convs = nn.Sequential(nn.Conv2d(args.history_length, 32, 5, stride=5, padding=1), nn.ReLU(),
-                                 nn.Conv2d(32, 64, 5, stride=5), nn.ReLU())
+      self.convs = nn.Sequential(nn.Conv2d(args.history_length, 32, 5, stride=5, padding=0), nn.ReLU(),
+                                 nn.Conv2d(32, 64, 5, stride=5, padding=0), nn.ReLU())
       self.conv_output_size = 576
     self.fc_h_v = NoisyLinear(self.conv_output_size, args.hidden_size, std_init=args.noisy_std)
     self.fc_h_a = NoisyLinear(self.conv_output_size, args.hidden_size, std_init=args.noisy_std)
