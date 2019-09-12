2188966cfb34dd9c3d6996cd90d0ab98494c6e79
Kai Arulkumaran <Kaixhin@users.noreply.github.com>
2019-09-12 17:46:32 +0100

    Note that DM's evaluation procedure is different

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ parser.add_argument('--learn-start', type=int, default=int(20e3), metavar='STEPS
 parser.add_argument('--evaluate', action='store_true', help='Evaluate only')
 parser.add_argument('--evaluation-interval', type=int, default=100000, metavar='STEPS', help='Number of training steps between evaluations')
 parser.add_argument('--evaluation-episodes', type=int, default=10, metavar='N', help='Number of evaluation episodes to average over')
+# TODO: Note that DeepMind's evaluation method is running the latest agent for 500K frames ever every 1M steps
 parser.add_argument('--evaluation-size', type=int, default=500, metavar='N', help='Number of transitions to use for validating Q')
 parser.add_argument('--render', action='store_true', help='Display screen (testing only)')
 parser.add_argument('--enable-cudnn', action='store_true', help='Enable cuDNN (faster but nondeterministic)')
